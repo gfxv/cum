79ebcbca59d798603d3340b986f0f7235f84faab
gfxv <superfreezu@gmail.com>
2024-12-09 19:59:54 +0300

    feat: generate video from qr codes
    generate qr codes from input file and
    create video using ffmpeg.

--- main.go
+++ main.go
@@ -1,25 +1,42 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
 	"io"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 
 	"github.com/skip2/go-qrcode"
 )
 
+//go:embed ffmpeg-flags
+var ffmpegFlags string
+
 const bufferSize = 2048
 const qrSize = 1024
 const filename = "test.txt"
+const outputFile = "output.mp4"
 const qrDir = "./qr"
 
 func main() {
 
+	if err := generateQR(filename); err != nil {
+		panic(err.Error())
+	}
+
+	if err := generateVideo(); err != nil {
+		panic(err.Error())
+	}
+
+}
+
+func generateQR(filename string) error {
 	file, err := os.Open(filename)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	defer file.Close()
 
@@ -28,20 +45,29 @@ func main() {
 	for {
 		n, err := file.Read(buffer)
 		if err != nil && err != io.EOF {
-			panic(err)
+			return err
 		}
 
 		if n == 0 {
 			break
 		}
 
-		fmt.Printf("read %d bytes: %s\n", n, string(buffer[:n]))
-		qrName := fmt.Sprintf("%d-%s.png", indx, strings.Split(filename, ".")[0])
+		qrName := fmt.Sprintf("file-%d.png", indx)
 		qrPath := filepath.Join(qrDir, qrName)
 		if err = qrcode.WriteFile(string(buffer), qrcode.Medium, qrSize, qrPath); err != nil {
-			panic(err)
+			return err
 		}
 		indx++
 	}
+	return nil
+}
 
+func generateVideo() error {
+	flags := strings.Split(strings.Trim(string(ffmpegFlags), "\n"), " ")
+	args := append(flags, outputFile)
+	cmd := exec.Command("ffmpeg", args...)
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	return nil
 }
